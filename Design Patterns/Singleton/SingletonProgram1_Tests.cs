6f8a632cf556197f2fd4d2356c41bbca3b52572a
unknown <hanohj@gmail.com>
2019-08-22 08:53:50 +0300

    Added tests + separated into folders

---
+++ Design Patterns/Singleton/SingletonProgram1_Tests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace Design_Patterns.Singleton
+{
+    [TestFixture]
+    public class SingletonTest
+    {
+        [Test]
+        public void IsSingleTonTest()
+        {
+            var db = SingletonDatabase.Instance;
+            var db2 = SingletonDatabase.Instance;
+            Assert.That(db, Is.SameAs(db2));
+            Assert.That(SingletonDatabase.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void SingletonTotalPopulationTest()
+        {
+            var rf = new SingletonRecordFinder();
+            var names = new[]
+            {
+                "Seoul", "Mexico City"
+            };
+            int tp = rf.GetTotalPopulation(names);
+            Assert.That(tp, Is.EqualTo(17500000 + 17400000));
+        }
+
+        [Test]
+        public void ConfigurablePopulationTest()
+        {
+            var rf = new ConfigurableRecordFinder(new DummyDatabase());
+            var names = new[] {"alpha", "gamma"};
+            int tp = rf.GetTotalPopulation(names);
+            Assert.That(tp , Is.EqualTo(4));
+        }
+    }
+}
