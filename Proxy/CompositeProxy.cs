48f530171414f42003ec210dd4093438eb1c5345
unknown <hanohj@gmail.com>
2019-09-15 07:24:00 +0300

    Added Decorator Pattern

---
+++ Proxy/CompositeProxy.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proxy
+{
+
+    // Proxy for performance improvment
+
+    class Creature
+    {
+        public byte Age;
+        public int X, Y;
+    }
+
+    class Creatures
+    {
+        public readonly int size;
+        private byte[] age;
+        private int[] x, y;
+
+        public Creatures(int size)
+        {
+            this.size = size;
+            this.age = new byte[size];
+            this.x = new int[size];
+            this.y = new int[size];
+        }
+
+        public struct CreatureProxy
+        {
+            private readonly Creatures _creatures;
+            private readonly int index;
+
+            public CreatureProxy(Creatures creatures, int index)
+            {
+                _creatures = creatures;
+                this.index = index;
+            }
+
+            public ref byte Age => ref _creatures.age[index];
+            public ref int X => ref _creatures.x[index];
+            public ref int Y => ref _creatures.y[index];
+        }
+
+        public IEnumerator<CreatureProxy> GetEnumerator()
+        {
+            for (int pos = 0; pos < size; ++pos)
+            {
+                yield return new CreatureProxy(this, pos);
+            }
+        }
+    }
+
+    class CompositeProxy
+    {
+        static void Main(string[] args)
+        {
+            var creatures = new Creatures(100);
+
+            foreach (var c in creatures)
+            {
+                c.X++;
+            }
+
+            var creatures2 = new Creatures(100);
+            foreach (Creatures.CreatureProxy c in creatures2)
+            {
+                c.X++;
+            }
+        }
+
+    }
+}
---
+++ Proxy/ValueProxy.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+/**
+ *  Motivation of using the Proxy pattern
+ *  Here we create a wrapper in order to use Percentages , addition and multiplication
+ */
+
+namespace DotNetDesignPatternDemos.Structural.Proxy.ValueProxy
+{
+
+    // Percentage class with logic 
+    public struct Percentage
+    {
+        public bool Equals(Percentage other)
+        {
+            return value.Equals(other.value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Percentage other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return value.GetHashCode();
+        }
+
+        private readonly float value;
+
+        public Percentage(float value)
+        {
+            this.value = value;
+        }
+
+        public static float operator *(float f, Percentage p)
+        {
+            return f * p.value;
+        }
+
+        public static Percentage operator +(Percentage a, Percentage b)
+        {
+            return new Percentage(a.value + b.value);
+        }
+
+        public override string ToString()
+        {
+            return $"{value * 100}%";
+        }
+    }
+
+
+
+    // Extensions functions 
+    public static class PercentageExtensions
+    {
+        public static Percentage Percent(this float value)
+        {
+            return new Percentage(value / 100.0f);
+        }
+
+        public static Percentage Percent(this int value)
+        {
+            return new Percentage(value / 100.0f);
+        }
+    }
+
+
+    // Demo
+    class ValueProxy
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine(
+                10f * 5.Percent()
+            );
+
+            Console.WriteLine(
+                2.Percent() + 3.Percent() // 5%
+            );
+        }
+    }
+}
