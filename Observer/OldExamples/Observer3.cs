e67b3011d0b20aef49ad5ab6d30254a76b1d0086
unknown <hanohj@gmail.com>
2019-09-15 07:30:34 +0300

    Created projects for Observer & Visitor

--- Observer/OldExamples/Observer3.cs
+++ Observer/OldExamples/Observer3.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
-using Design_Patterns.Annotations;
 
 namespace Design_Patterns.Observer
 {
